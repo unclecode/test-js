477597bdd6f7dc86e8978097d594066dfaf52310
unclecode <unclecode@kidocode.com>
2019-12-12 09:33:39 +0800

    initial commit for 003_debugging/002_understanding_the_differences_between_the_freecodecamp_and_browser_console

---
+++ 3_debugging/2_understanding_the_differences_between_the_freecodecamp_and_browser_console/test_2.js
@@ -0,0 +1,4 @@
+/**
+You should use <code>console.clear()</code> to clear the browser console.
+You should use <code>console.log()</code> to print the <code>output</code> variable.
+*/
---
+++ README.md
@@ -0,0 +1,2 @@
+## 003_debugging
+# 002_understanding_the_differences_between_the_freecodecamp_and_browser_console
