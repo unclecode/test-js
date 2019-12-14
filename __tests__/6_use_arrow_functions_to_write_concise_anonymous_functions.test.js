e599aa17fcb2614e36b1c9ed33eb50e29572b498
unclecode <unclecode@kidocode.com>
2019-12-14 10:31:46 +0800

    Adding tests and package.json

---
+++ __tests__/6_use_arrow_functions_to_write_concise_anonymous_functions.test.js
@@ -0,0 +1,17 @@
+describe(`Lesson 6 use arrow functions to write concise anonymous functions`, () =>{
+	test(`User should replace <code>var</code> keyword.`, () =>{
+		//test goes here
+	})
+	test(`<code>magic</code> should be a constant variable (by using <code>const</code>).`, () =>{
+		//test goes here
+	})
+	test(`<code>magic</code> should be a <code>function</code>.`, () =>{
+		//test goes here
+	})
+	test(`<code>magic()</code> should return correct date.`, () =>{
+		//test goes here
+	})
+	test(`<code>function</code> keyword should not be used.`, () =>{
+		//test goes here
+	})
+})
---
+++ __tests__/7_write_arrow_functions_with_parameters.test.js
@@ -0,0 +1,17 @@
+describe(`Lesson 7 write arrow functions with parameters`, () =>{
+	test(`You should replace the <code>var</code> keyword.`, () =>{
+		//test goes here
+	})
+	test(`<code>myConcat</code> should be a constant variable (by using <code>const</code>).`, () =>{
+		//test goes here
+	})
+	test(`<code>myConcat</code> should be a function.`, () =>{
+		//test goes here
+	})
+	test(`<code>myConcat()</code> should return <code>[1, 2, 3, 4, 5]</code>.`, () =>{
+		//test goes here
+	})
+	test(`<code>function</code> keyword should not be used.`, () =>{
+		//test goes here
+	})
+})
