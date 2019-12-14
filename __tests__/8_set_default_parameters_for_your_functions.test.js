4f074593b197ec7e197e6d76fd7653dccf4e9147
unclecode <unclecode@kidocode.com>
2019-12-14 10:31:46 +0800

    Adding tests and package.json

---
+++ __tests__/8_set_default_parameters_for_your_functions.test.js
@@ -0,0 +1,11 @@
+describe(`Lesson 8 set default parameters for your functions`, () =>{
+	test(`The result of <code>increment(5, 2)</code> should be <code>7</code>.`, () =>{
+		//test goes here
+	})
+	test(`The result of <code>increment(5)</code> should be <code>6</code>.`, () =>{
+		//test goes here
+	})
+	test(`A default parameter value of <code>1</code> should be used for <code>value</code>.`, () =>{
+		//test goes here
+	})
+})
