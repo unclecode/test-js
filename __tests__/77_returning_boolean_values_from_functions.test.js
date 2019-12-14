54d188434b142c106855c6d99c763d76fa30d7c5
unclecode <unclecode@kidocode.com>
2019-12-14 10:31:44 +0800

    Adding tests and package.json

---
+++ __tests__/77_returning_boolean_values_from_functions.test.js
@@ -0,0 +1,11 @@
+describe(`Lesson 77 returning boolean values from functions`, () =>{
+	test(`<code>isLess(10,15)</code> should return <code>true</code>`, () =>{
+		//test goes here
+	})
+	test(`<code>isLess(15,10)</code> should return <code>false</code>`, () =>{
+		//test goes here
+	})
+	test(`You should not use any <code>if</code> or <code>else</code> statements`, () =>{
+		//test goes here
+	})
+})
---
+++ __tests__/78_return_early_pattern_for_functions.test.js
@@ -0,0 +1,20 @@
+describe(`Lesson 78 return early pattern for functions`, () =>{
+	test(`<code>abTest(2,2)</code> should return a number`, () =>{
+		//test goes here
+	})
+	test(`<code>abTest(2,2)</code> should return <code>8</code>`, () =>{
+		//test goes here
+	})
+	test(`<code>abTest(-2,2)</code> should return <code>undefined</code>`, () =>{
+		//test goes here
+	})
+	test(`<code>abTest(2,-2)</code> should return <code>undefined</code>`, () =>{
+		//test goes here
+	})
+	test(`<code>abTest(2,8)</code> should return <code>18</code>`, () =>{
+		//test goes here
+	})
+	test(`<code>abTest(3,3)</code> should return <code>12</code>`, () =>{
+		//test goes here
+	})
+})
